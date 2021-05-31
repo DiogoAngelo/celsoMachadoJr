9000f0ad01ab6eda5c0e812d52d25a22f791a183
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-05-31 21:16:44 +0100

    update

---
+++ js/menu-burger.js
@@ -0,0 +1,15 @@
+const burgerTrace = document.querySelectorAll('.trace');
+const menuBurger = document.querySelector('.burger');
+const menu = document.querySelector('.menu-grow');
+const menuGrowItems = document.querySelector('.menu-grow-items');
+
+function openMenu() {
+    burgerTrace.forEach((trace) => {
+        trace.classList.toggle('ativo');
+    });
+    menu.classList.toggle('ativo');
+    menuGrowItems.classList.toggle('ativo');
+
+}
+
+menuBurger.addEventListener('click', openMenu);
