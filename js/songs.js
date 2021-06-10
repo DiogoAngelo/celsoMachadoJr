427d84c53f0fc749aa12b651a8dbc3279fdace85
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-06-10 11:14:43 +0100

    update songlist

--- js/songs.js
+++ js/songs.js
@@ -1,18 +1,20 @@
-const arraySongs = ['<iframe width="415" height="415" src="https://www.youtube.com/embed/_oI_rZE1UWY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/OjbYDq1ATMc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/U51Ul6iyMAA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/2xhT82R3DkQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/RrcGvbikMeM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/SJpSu8SVd1E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/JkyczBrrQL8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/4ggZQjtH78U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/RcEmQ2Q7f94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-'<iframe width="415" height="415" src="https://www.youtube.com/embed/KH0UEvJ_ES8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>']
+const arraySongs = [
+    "https://www.youtube.com/embed/_oI_rZE1UWY", 
+    "https://www.youtube.com/embed/OjbYDq1ATMc",
+    "https://www.youtube.com/embed/U51Ul6iyMAA",
+    "https://www.youtube.com/embed/2xhT82R3DkQ",
+    "https://www.youtube.com/embed/RrcGvbikMeM",
+    "https://www.youtube.com/embed/SJpSu8SVd1E",
+    "https://www.youtube.com/embed/JkyczBrrQL8",
+    "https://www.youtube.com/embed/4ggZQjtH78U",
+    "https://www.youtube.com/embed/RcEmQ2Q7f94",
+    "https://www.youtube.com/embed/KH0UEvJ_ES8"
+]
 
-const album = document.querySelector('.album');
+const iframe = document.querySelector('iframe');
 
 function playSong(index) {
-    album.innerHTML = arraySongs[index];
+    iframe.src = arraySongs[index];
 }
 
 const faixas = document.querySelectorAll('.track');
