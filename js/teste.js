6b2fff559853b9dd381e49781d86ef41fb303197
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-06-10 19:16:20 +0100

    teste.js

---
+++ js/teste.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('.header-grid');
+const btn = document.querySelector('.button');
+
+async function handleClick() {
+    const imagem = await fetch('./img/capa-color.png');
+
+    const finalImg = imagem.url;
+    addColor(finalImg);
+}
+function addColor(finalImg) {
+    header.setAttribute('style', `background-image: url(${finalImg})`);
+}
+
+btn.addEventListener('click', handleClick);
+
