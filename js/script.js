986f579341db37d4f17ccf7c8391b042976c4e6e
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-06-19 21:34:54 +0100

    adjusts

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 import initAddColor from './add-color.js';
 import initMenuBar from './menu-bar.js';
 import initMenuBurger from './menu-burger.js';
-import initFlow from './pageFlow.js';
 
 initAddColor();
 initMenuBar();
 initMenuBurger();
-initFlow();
